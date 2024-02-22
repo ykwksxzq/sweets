be788d0cf6f1192e3e18e33438dab4cb58da2365
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2024-02-22 17:35:00 +0000

    Bootstrapの導入

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
 
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create!(
+  email: "aaabbb@mail.com", ## 任意のメールアドレス,
+  password: "aaabbb" ## 任意のパスワード
+)
