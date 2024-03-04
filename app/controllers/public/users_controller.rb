f1c2eafe5087fc717e2373d6c76814cec89157f3
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2024-03-04 15:17:56 +0000

    マイページ編集

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -5,5 +5,25 @@ class Public::UsersController < ApplicationController
     @posts = Post.published.where(user_id: params[:id]).page(params[:page]).per(4).order(created_at: :desc)
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      flash[:notice] = "会員情報の更新が完了しました"
+      redirect_to mypage_path(@user.id)
+    else
+      flash.now[:alert] = "登録できませんでした。お手数ですが、入力内容をご確認の上再度お試しください"
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :email, :profile_image)
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,17 +19,34 @@ class User < ApplicationRecord
      file_path = Rails.root.join('app/assets/images/no_image.jpg')
      profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
    end
-     profile_image
+     profile_image.variant(resize_to_limit: [width, height]).processed
   end
+  
+  GUEST_MEMBER_EMAIL = "guest@example.com"
 
   #ゲストログイン用
   def self.guest
-    find_or_create_by!(email: 'guest@example.com') do |user|
+    find_or_create_by!(email: GUEST_MEMBER_EMAIL ) do |user|
       user.password = SecureRandom.urlsafe_base64
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
       # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
     user.name = "ゲスト"
     end
   end
+  
+  def guest_user?
+    email == GUEST_MEMBER_EMAIL
+  end
+  
+  def owns?(other_user)
+    self == other_user
+  end
 
+  # current_user 且つ guest_user でなかったら
+  # if @user == current_user && !current_user.guest_user?
+
+  def matches_current_user?(other_user)
+    self == other_user && !other_user.guest_user?
+  end
+  
 end
