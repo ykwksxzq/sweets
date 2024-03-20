7b613b08647e247f7907830be74568977f9b8ab0
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2024-03-20 13:47:46 +0000

    レイアウトの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,13 +54,10 @@ class User < ApplicationRecord
   
   # フォローしている関連付け
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  
   # フォローされている関連付け
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  
   # フォローしているユーザーを取得
   has_many :followings, through: :active_relationships, source: :followed
-  
   # フォロワーを取得
   has_many :followers, through: :passive_relationships, source: :follower
   
