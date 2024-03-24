b15711f8261d8ed9067871544b86bef7e5f72bdb
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2024-03-24 05:54:21 +0000

    レイアウトの修正

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -5,7 +5,7 @@ class Public::UsersController < ApplicationController
   def index
     @users = User.all.page(params[:page]).per(10)
   end
-
+  
   def show
     @user = User.find(params[:id])
     @posts = Post.published.where(user_id: params[:id]).page(params[:page]).per(4).order(created_at: :desc)
